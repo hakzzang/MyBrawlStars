17ac4db071bac3012411bbd2dd6afbdffcf65ee9
hakzzang <mbh930312@naver.com>
2020-07-26 21:44:30 +0900

    Added simple ui test code with hilt

--- app/src/androidTest/java/com/hbs/mybrawlstars/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/hbs/mybrawlstars/ExampleInstrumentedTest.kt
@@ -1,12 +1,18 @@
 package com.hbs.mybrawlstars
 
-import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import androidx.test.rule.ActivityTestRule
+import com.hbs.mybrawlstars.ui.main.MainActivity
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.RuleChain
 import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -14,11 +20,22 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 @RunWith(AndroidJUnit4::class)
+@HiltAndroidTest
 class ExampleInstrumentedTest {
+    @get:Rule
+    var testRule = RuleChain.outerRule(HiltAndroidRule(this))
+        .around(ActivityTestRule(MainActivity::class.java))
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.hbs.mybrawlstars", appContext.packageName)
+    fun `메인액티비티_초기화면이_보이는지_테스트`() {
+        Espresso.onView(
+            withId(R.id.tv_information_api_test)
+        ).check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
     }
+
+    @Test
+    fun `메인액티비티에_TITLE이_출력되는지_테스트`(){
+        Espresso.onView(withId(R.id.tv_title)).check(ViewAssertions.matches(ViewMatchers.withText("TITLE")))
+    }
+
 }
